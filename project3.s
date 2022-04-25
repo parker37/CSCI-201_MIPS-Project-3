62b498cb8b71a4cfda389232821d3fce16542e9c
Ronte's Laptop <ronte.parker@bison.howard.edu>
2022-04-25 19:19:45 -0400

    Send string to sub_a and save return address in stack

--- project3.s
+++ project3.s
@@ -8,14 +8,16 @@ main:
 	la $a0, input			# loads address of input variable
 	li $a1, 1001			# caps how many characters to accept before cutting input off
 	syscall					# performs action: take in user input
+	
+	jal sub_a				# goes into sub_a passing input string
 
 
 # takes in the string and sends substrings to sub_b to get the decimal value or error msg back
 # prints returned results from sub_b
 sub_a:
-
-
-
+	addi $sp, $sp, -4		# create space in stack for return address
+	sw $ra, 0($sp)			# store return address into stack
+	
 
 
 # takes in each substring and loops through the characters to evaluate the result
