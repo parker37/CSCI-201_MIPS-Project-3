f2ca668561cc431dcccac6f14abc0b68dd44504e
Ronte's Laptop <ronte.parker@bison.howard.edu>
2022-04-25 19:13:55 -0400

    Read Input String

--- project3.s
+++ project3.s
@@ -1,12 +1,13 @@
 .data
-
+	input: .space 1001			# makes space for user input
 
 .text
-
-
 # Calls sub_a and passes the string in via the stack
 main:
-
+	li $v0, 8				# load syscode for getting input
+	la $a0, input			# loads address of input variable
+	li $a1, 1001			# caps how many characters to accept before cutting input off
+	syscall					# performs action: take in user input
 
 
 # takes in the string and sends substrings to sub_b to get the decimal value or error msg back
