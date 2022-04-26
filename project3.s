930bcd6481df107edf7d04b26f5e872acc34645f
Ronte's Laptop <ronte.parker@bison.howard.edu>
2022-04-25 22:05:40 -0400

    Substring loop

--- project3.s
+++ project3.s
@@ -1,5 +1,5 @@
 .data
-	input: .space 1001			# makes space for user input
+	input: .space 1001		# makes space for user input
 
 .text
 # Calls sub_a and passes the string in via the stack
@@ -15,23 +15,27 @@ main:
 # takes in the string and sends substrings to sub_b to get the decimal value or error msg back
 # prints returned results from sub_b
 sub_a:
-	addi $sp, $sp, -4		# create space in stack for return address
-	sw $ra, 0($sp)			# store return address into stack
+	addi $sp, $sp, -4			# create space in stack for return address
+	sw $ra, 0($sp)				# store return address into stack
 	
-	la $t0 ($a0)			# save input string to new register
-	li $t2, 0				# count var for substring length
-	loop:					# loop for separating string into substrings
-	la $t1 0($t0)			# load char into temp var
-	beq $t1, 59, sub_program
-	
+	la $t0 ($a0)				# save input string to new register
+	li $t2, 0					# count var for substring length
+	loop:						# loop for separating string into substrings
+	la $t1 0($t0)				# load char into temp var
+	beq $t1, 0, return			# if '\n' jump to sub_b then return back to main
+	beq $t1, 59, sub_program	# if ';' jump to sub_b to get value
+	addi $t2, $t2, 1			# increment count
 	
 	sub_program:
-	jal sub_b				# goes into sub_B passing input substring
+	la $a1, ($t2)				# stores length of substring in $a1
+	jal sub_b					# goes into sub_B passing input substring
 	j loop
 	
 	return:
 
 # takes in each substring and loops through the characters to evaluate the result
+# a0: substring
+# a1: length of string
 # returns decimal value or error msg 
 sub_b:
 
