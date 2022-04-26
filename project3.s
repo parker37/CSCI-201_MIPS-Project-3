bfcb70c2c70b19bd123f928916c74a39719b3858
Ronte's Laptop <ronte.parker@bison.howard.edu>
2022-04-25 22:40:30 -0400

    Substring loop: prep substring for subprogram

--- project3.s
+++ project3.s
@@ -18,19 +18,24 @@ sub_a:
 	addi $sp, $sp, -4			# create space in stack for return address
 	sw $ra, 0($sp)				# store return address into stack
 	
-	la $t0 ($a0)				# save input string to new register
+	la $s0 ($a0)				# save input string to new register
 	loop:						# loop for separating string into substrings
-	la $s0, input				# space for substring
+	la $t0, input				# space for substring
 	li $t2, 0					# count var for substring length
-	lb $t1 0($t0)				# load char into temp var
+	lb $t1 0($s0)				# load char into temp var
 	beq $t1, 0, return			# if '\n' jump to sub_b then return back to main
 	beq $t1, 59, sub_program	# if ';' jump to sub_b to get value
 	addi $t2, $t2, 1			# increment count
-	sb $t1, 0($s0)				# store char into substring register
-	addi $s0, $s0, 1			# moves pointer to next byte
+	sb $t1, 0($t0)				# store char into substring register
+	addi $t0, $t0, 1			# moves pointer to next byte
+	addi $s0, $s0, 1			# moves to next character in input string
 	
 	sub_program:
 	la $a1, ($t2)				# stores length of substring in $a1
+	la $a0, ($t0)				# load substring in $a0
+	li $t0, 0					# load '\n' char in $t0
+	sb $t0, 0($a0)				# store $t0 at the end of the substr in $a0
+	sub $a0, $a0, $a1			# put substr pointer at the beginning at the end of the word
 	jal sub_b					# goes into sub_B passing input substring
 								# print out value with ',' after return
 	j loop
